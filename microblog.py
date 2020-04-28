d71151bc086e5b7c3b7666c336677efb61af1606
famousdraw <famousdraw@163.com>
2020-04-28 11:23:45 +0800

    microblog.py

--- microblog.py
+++ microblog.py
@@ -1,9 +1,6 @@
-from app import app
+from app import app,db
+from app.models import User, Post
 
-#from flask import Flask
-#from config import Config
-
-#app = Flask(__name__)
-#app.config.from_object(Config)
-
-#from app import routes
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
